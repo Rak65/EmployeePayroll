096d4d3df6c867944efa0b5e84ef7ecc8236939c
Rak65 <rakeshp6505@gmail.com>
2023-06-09 21:07:28 +0530

    Insert the table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -10,3 +10,5 @@ Create table employee_payroll
 	Salary decimal(10,2),
 	Starting_date Date
 );
+--UC3
+Insert into employee_payroll (Emp_Name , Salary , Starting_date) values ('Ankit' , 65000 , '2023-04-10');
